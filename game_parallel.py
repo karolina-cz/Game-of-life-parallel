124ed9916c90d578f12e468bcfe6fa37b4bdcd2c
karolina-cz <karolina.czachorska@gmail.com>
2021-05-23 10:49:51 +0200

    Add display board after each iteration

--- game_parallel.py
+++ game_parallel.py
@@ -16,9 +16,8 @@ left,right = cartesian.Shift(direction = 0,disp=1)
 
 def main():
   sub_board, iterations, can_display_board, file_out = init_game()
-  print("initialized")
-  sub_board = compute_iterations(sub_board, iterations)
-  finalize(sub_board, can_display_board, file_out)
+  sub_board = compute_iterations(sub_board, iterations, can_display_board)
+  finalize(sub_board, file_out)
 
 
 def init_game():
@@ -36,31 +35,46 @@ def init_game():
     file_out = None
   sub_board = comm.scatter(board, root=0)  # wyslij procesom ich czesci tablicy 
   iterations = comm.bcast(iterations, root=0)  #wyslij procesom liczbe iteracji
+  can_display_board = comm.bcast(can_display_board, root=0)  #wyslij procesom liczbe iteracji
   return sub_board, iterations, can_display_board, file_out
 
 
-def compute_iterations(sub_board, iterations):
+def compute_iterations(sub_board, iterations, can_display_board):
   sub_board = process_communicator.add_redundant_rows(sub_board, right, left)  # wysylanie i dodawanie wierszy nadmiarowych
-  print("exchanged")
-  for i in range(0, iterations):
-    if process_utils.is_master(id):
-      print("iteration {}".format(i))
-    sub_board = board_utils.get_next_iteration(sub_board)
-    sub_board = process_communicator.exchange_data(sub_board, right, left)
-    comm.barrier()
-
-  sub_board = board_utils.remove_redundant_rows(sub_board, id, size) #usun nadmiarowe wiersze
+  if can_display_board == "yes":
+    sub_board = game_loop_with_display(sub_board, iterations)
+  else:
+    sub_board = game_loop_no_display(sub_board, iterations)
   return sub_board
 
-def finalize(sub_board, can_display_board, file_out):
+def finalize(sub_board, file_out):
   result = comm.gather(sub_board, root=0)
   if process_utils.is_master(id):
     result = np.concatenate(result[0:], axis=0)
-    print("Plansza policzona rownolegle")
-    if can_display_board == "yes":
-      board_utils.display_board(result)
     if file_out is not None:
       np.savetxt(file_out, result, delimiter=",")
 
 
+def game_loop_with_display(sub_board, iterations):
+  for i in range(0, iterations):
+      sub_board = board_utils.get_next_iteration(sub_board)
+      sub_board = process_communicator.exchange_data(sub_board, right, left)
+      temp_board = board_utils.remove_redundant_rows(sub_board, id, size) 
+      comm.barrier()
+      result = comm.gather(temp_board, root=0)
+      if process_utils.is_master(id):
+        result = np.concatenate(result[0:], axis=0)
+        print("computing iteration {}".format(i))
+        board_utils.display_board(result)
+  return sub_board
+
+def game_loop_no_display(sub_board, iterations):
+  for i in range(0, iterations):
+    if process_utils.is_master(id):
+      print("computing iteration {}".format(i))
+    sub_board = board_utils.get_next_iteration(sub_board)
+    sub_board = process_communicator.exchange_data(sub_board, right, left)
+    comm.barrier()
+  sub_board = board_utils.remove_redundant_rows(sub_board, id, size) #usun nadmiarowe wiersze
+  return sub_board
 main()
--- game_seq.py
+++ game_seq.py
@@ -3,20 +3,35 @@ import argparse
 import board_utils
 import args_parser
 import numpy as np
+import os
+from IPython.display import clear_output
 
 def main():
   args = args_parser.register_arg_parser()
   iterations = args_parser.get_iterations(args)
   board = args_parser.get_board(args)
 
-  for i in range (0, iterations):
-    print("computing iteration {}".format(i))
-    board = board_utils.get_next_iteration(board)
-
   if args.d == "yes":
-    board_utils.display_board(board)
+    board = game_loop_with_display(board, iterations)
+  else: 
+    board = game_loop_no_display(board, iterations)
   
   if args.file_out is not None:
       np.savetxt(args.file_out, board, delimiter=",")
+
+
+def game_loop_no_display(board, iterations):
+  for i in range (0, iterations):
+    print("computing iteration {}".format(i))
+    board = board_utils.get_next_iteration(board)
+  return board
+
+
+def game_loop_with_display(board, iterations):
+  for i in range (0, iterations):
+    board = board_utils.get_next_iteration(board)
+    board_utils.display_board(board)
+  return board
+
 
 main()
